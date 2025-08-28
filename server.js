d906e0a8f800d786828a0ed94288c48a528cce3e
zvn7 <aammaul7@gmail.com>
2025-08-28 09:16:09 +0700

    tatattaa

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // CORS setup
 app.use(
     cors({
-        origin: ["https://dev-swid.vercel.app", "http://localhost:5173"],
+        origin: "*",
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     })
 );
